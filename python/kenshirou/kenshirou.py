df1a18c07d67187530283704c442909027b4a8bb
t4t5u0 <ymmtryk0902@gmail.com>
2021-03-27 02:15:41 +0900

    [add] １行にした

--- python/kenshirou/kenshirou.py
+++ python/kenshirou/kenshirou.py
@@ -1,11 +1,1 @@
-
-
-# class Kenshirou:
-#     str: data
-
-
-def solve(n: int) -> str:
-    return bin(n).replace('0', 'た').replace('1', 'あ')[2:]
-
-
-print(solve(int(input('入力しなさい：'))))
+print(bin(int(input('入力しなさい：'))).replace('0', 'た').replace('1', 'あ')[2:])
