462a0f24b40a90f9c8a852cb9230892c0a8e6196
t4t5u0 <ymmtryk0902@gmail.com>
2021-03-27 02:18:00 +0900

    [add] １行にした

--- python/zenrekkyo/zenrekkyo.py
+++ python/zenrekkyo/zenrekkyo.py
@@ -1,7 +1,3 @@
 from itertools import combinations_with_replacement
 
-n = int(input("桁数："))
-ls = input("要素を空白区切りで：").split()  
-
-for item in combinations_with_replacement(ls, n):
-    print(''.join(item))
+print(*(''.join(item) for item in combinations_with_replacement(input("要素を空白区切りで：").split(), int(input("桁数：")))), sep='\n')
